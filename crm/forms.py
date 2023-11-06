f9ebd0c466536654c37e00c10cc9b64092361722
milana <milanaroy401@gmail.com>
2023-11-06 12:45:55 +0530

    empcrm

---
+++ crm/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from crm.models import Employees
+class EmployeeForm(forms.Form):
+    name=forms.CharField()
+    department=forms.CharField()
+    salary=forms.IntegerField()
+    email=forms.EmailField()
+    age=forms.IntegerField()
+    contact=forms.CharField()
+class EmployeeModelForm(forms.ModelForm):
+    class Meta:
+        model=Employees
+        fields="__all__"
+
---
+++ crm/views.py
@@ -0,0 +1,35 @@
+from typing import Any
+from django.shortcuts import render,redirect
+from django.views.generic import View
+from crm.forms import EmployeeForm,EmployeeModelForm
+from crm.models import Employees
+class EmployeeCreateView(View):
+    def get(self,request,*args,**kwargs):
+        form=EmployeeModelForm()
+        return render(request,"emp_add.html",{"form":form})
+    def post(self,request,*args,**kwargs):
+        form=EmployeeModelForm(request.POST)
+        if form.is_valid():
+            form.save
+            print(form.cleaned_data)
+            # Employees.objects.create(**form.cleaned_data)
+            print("created")
+            return render(request,"emp_add.html",{"form":form})
+        else:
+            return render(request,"emp_add.html",{"form":form})
+class EmployeeListView(View):
+    def get(self,request,*args,**kwargs):
+        qs=Employees.objects.all()
+        return render(request,"emp_list.html",{"data":qs})
+
+class EmployeeDetailView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        qs=Employees.objects.get(id=id)
+        return render(request,"emp_detail.html",{"data":qs})
+class EmployeeDeleteView(View):
+    def get(self,request,*args,**kwargs):
+        id=kwargs.get("pk")
+        qs=Employees.objects.get(id=id).delete()
+        return redirect("emp-all")
+    
